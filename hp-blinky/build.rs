6050f8b575c64ed63a0d794d6d4257a413806126
Leigh Oliver <leighcharlesoliver@gmail.com>
2026-03-04 09:02:23 +1100

    Start making the main core program, which will host ulp-blinky

--- hp-blinky/build.rs
+++ hp-blinky/build.rs
@@ -1,7 +1,15 @@
-fn main() {
+use std::error::Error;
+use esp_metadata_generated::Chip;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Determine the name of the configured device:
+    let chip = Chip::from_cargo_feature()?;
+    // Define all necessary configuration symbols for the configured device:
+    chip.define_cfgs();
     linker_be_nice();
     // make sure linkall.x is the last linker script (otherwise might cause problems with flip-link)
     println!("cargo:rustc-link-arg=-Tlinkall.x");
+    Ok(())
 }
 
 fn linker_be_nice() {

cbcb4d5a550c244e9ff1209992ba0130450753e0
Leigh Oliver <leighcharlesoliver@gmail.com>
2026-03-23 08:58:07 +1100

    Start-up interrupt resets counter. Program runs for 3 seconds before reset. Button press interrupt increments counter while running.

--- ulp-baremetal/src/main.rs
+++ ulp-baremetal/src/main.rs
@@ -15,15 +15,27 @@ use esp_lp_hal::gpio::Input;
 
 const ADDRESS: u32 = 0x1000;
 
-/// Default interrupt handler for RTC Peripheral interrupts (SENS).
-/// Users may override SensInterrupt to change this behaviour.
+/// Override interrupt handler for RTC Peripheral interrupts (SENS).
+#[unsafe(export_name = "SensInterrupt")]
+pub fn sens_interrupt(sar_cocpu_int_st: esp_lp_hal::pac::sens::sar_cocpu_int_st::R) {
+    // Did we get a startup interrupt? If so, reset counter to 0x0
+    if sar_cocpu_int_st.sar_cocpu_start_int_st().bit_is_set() {
+        let ptr = ADDRESS as *mut u32;
+        unsafe { ptr.write_volatile(0); }
+    }
+}
+
+/// Overriden handler for GPIO interrupts
 #[unsafe(export_name = "GpioInterrupt")]
-pub fn gpio_interrupt(_pin_status : u32) {
-    let ptr = ADDRESS as *mut u32;
-    // unsafe { ptr.write_volatile(pin_status) };
-    let mut i : u32 = unsafe { ptr.read_volatile() };
-    i = i.wrapping_add(1u32);
-    unsafe { ptr.write_volatile(i); }
+pub fn gpio_interrupt(pin_status : u32) {
+    // Will increment counter when GPIO 5 is pressed
+    let pin_number = 5;
+    if (pin_status & (1 << pin_number)) != 0 {
+        let ptr = ADDRESS as *mut u32;
+        let mut i : u32 = unsafe { ptr.read_volatile() };
+        i = i.wrapping_add(1u32);
+        unsafe { ptr.write_volatile(i); }
+    }
 }
 
 // Hackily enable a GPIO interrupt 
@@ -49,19 +61,20 @@ fn enable_gpio_intr() {
     unsafe { &*esp_lp_hal::pac::RTC_IO::PTR }.pin5().write(|w| unsafe { w.int_type().bits(io_int_type) });
 }
 
+// Hackily enable sens interrupt
+fn enable_sens_intr() {
+    unsafe { &*esp_lp_hal::pac::SENS::PTR }.sar_cocpu_int_ena().write(|w| w.sar_cocpu_start_int_ena().set_bit());
+}
+
 #[entry]
 fn main(mut _stomp_pin : Input<5>) {
+    enable_sens_intr();
     enable_gpio_intr();
 
     let dly = Delay{};
-    // let ptr = ADDRESS as *mut u32;
 
-    loop {
-        // if stomp_pin.is_low().unwrap() {
-        //     let mut i : u32 = unsafe { ptr.read_volatile() };
-        //     i = i.wrapping_add(1u32);
-        //     unsafe { ptr.write_volatile(i); }
-        // }
-        dly.delay_millis(100);
-    }
+    // Handle interupts for 3 seconds, then reset.
+    // This should trigger the 'start up' interrupt from SensInterrupt,
+    // which should reset the counter to 0>
+    dly.delay_millis(3000);
 }
